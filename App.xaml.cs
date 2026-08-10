94511ca2577948dbd2bda8fdba013c4fc94ddcbb
GMTPC <ghostminhtoan@gmail.com>
2026-08-10 18:29:39 +0700

    ponyman: remove strict single instance mutex check to allow uninterrupted app launch

--- App.xaml.cs
+++ App.xaml.cs
@@ -27,19 +27,7 @@ namespace get_link_manga
         protected override void OnStartup(StartupEventArgs e)
         {
             PortableRuntimeBootstrap.EnsurePortableRuntime();
-            try
-            {
-                _singleInstanceMutex = new Mutex(true, BuildSingleInstanceMutexName(), out bool createdNew);
-                if (!createdNew)
-                {
-                    TryActivateExistingInstance();
-                    Shutdown();
-                    return;
-                }
-            }
-            catch
-            {
-            }
+
 
             // Tự động kiểm tra và cài đặt Cloudflare Warp nếu chưa có
             EnsureCloudflareWarpInstalled();
--- BuildInfo.cs
+++ BuildInfo.cs
@@ -2,6 +2,6 @@
 {
 public static class BuildInfo
 {
-public const string DisplayText = "2026-08-10 ∕ 06.24.37 PM";
+public const string DisplayText = "2026-08-10 ∕ 06.29.12 PM";
 }
 }
